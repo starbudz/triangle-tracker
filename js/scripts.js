c81e32bbb617a3a247bb0837b8876f67110db438
starbudz <victorngigi@gmail.com>
2018-08-10 10:52:48 +0300

    added function to recieve user input on triangle sides

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var sideA = parseInt(prompt("Enter the length of side A in centimeters"));
+if (isNaN(sideA)){
+  alert("enter a real number");
+}
+var sideB = parseInt(prompt("Enter the length of side B in centimeters"));
+if (isNaN(sideB)){
+  alert("enter a real number");
+}
+var sideC = parseInt(prompt("Enter the length of side C in centimeters"));
+if (isNaN(sideC)){
+  alert("enter a real number");
+}
