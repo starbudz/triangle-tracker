ae684a5f1a67f91f91ad212cc7b8e57f3f1d8c23
starbudz <victorngigi@gmail.com>
2018-08-10 15:52:18 +0300

    fixed bug where there were no prompts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+var triangleTracker = function(triangle){
 var triangle= new Array(3);
 
 var dimmensions=0;
@@ -6,7 +7,6 @@ for (dimmensions=0; dimmensions<3; dimmensions++)
 {
 triangle[dimmensions]=(parseInt(prompt('Enter the dimmensions in centimeters!',' ')));
 }
-
 if(triangle[0] === triangle[1] && triangle[0] === triangle[2] && triangle[1] === triangle[2]){
 alert("This triangle is Equilateral");
 }
@@ -16,6 +16,8 @@ alert("This triangle is Isosceles");
 else if((triangle[0] + triangle[1]) <= triangle[2] || (triangle[0] + triangle[2]) <= triangle[1] || (triangle[1] + triangle[2]) <= triangle[0]) {
 alert("This is not a Triangle");
 }
-else{
+else if(triangle[0] !== triangle[1] && triangle[0] !== triangle[2] && triangle[1] !== triangle[2]){
 alert("This is a Scalene triangle");
 }
+}
+triangleTracker();
