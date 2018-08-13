cafcc5f1e5e790f4ee8586061be6aee32e113fe6
starbudz <victorngigi@gmail.com>
2018-08-13 09:25:35 +0300

    fixed indentation errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
-var triangleTracker = function(triangle){
-var triangle= new Array(3);
+var triangleTracker = function(triangle) {
+  var triangle = new Array(3);
 
-var dimmensions=0;
+  var dimmensions = 0;
 
-for (dimmensions=0; dimmensions<3; dimmensions++){
-triangle[dimmensions]=(parseFloat(prompt('Enter the dimmensions in centimeters!',' ')));
-}
-if(triangle[0] === triangle[1] && triangle[0] === triangle[2] && triangle[1] === triangle[2]){
-alert("This triangle is Equilateral");
-}
-else if(triangle[0] === triangle[1] || triangle[0]=== triangle[2] || triangle[1] === triangle[2]){
-alert("This triangle is Isosceles");
-}
-else if((triangle[0] + triangle[1]) <= triangle[2] || (triangle[0] + triangle[2]) <= triangle[1] || (triangle[1] + triangle[2]) <= triangle[0]) {
-alert("This is not a Triangle");
-}
-else if(triangle[0] !== triangle[1] && triangle[0] !== triangle[2] && triangle[1] !== triangle[2]){
-alert("This is a Scalene triangle");
-}
+  for (dimmensions = 0; dimmensions < 3; dimmensions++) {
+    triangle[dimmensions] = (parseFloat(prompt('Enter the dimmensions in centimeters!', ' ')));
+  }
+  if (triangle[0] === triangle[1] && triangle[0] === triangle[2] && triangle[1] === triangle[2]) {
+    alert("This triangle is Equilateral");
+  } else if (triangle[0] === triangle[1] || triangle[0] === triangle[2] || triangle[1] === triangle[2]) {
+    alert("This triangle is Isosceles");
+  } else if ((triangle[0] + triangle[1]) <= triangle[2] || (triangle[0] + triangle[2]) <= triangle[1] || (triangle[1] + triangle[2]) <= triangle[0]) {
+    alert("This is not a Triangle");
+  } else if (triangle[0] !== triangle[1] && triangle[0] !== triangle[2] && triangle[1] !== triangle[2]) {
+    alert("This is a Scalene triangle");
+  }
 }
